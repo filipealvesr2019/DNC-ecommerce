e1ffa120c3370483cdae077c9fb4bd2e0523a1db
Filipe Alves <filipealvesrds2019@gmail.com>
2023-06-08 23:23:39 -0300

    cm

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Login from './views/Login/Login.jsx';
+import Home from './views/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+/*
+  {
+    path: "/products",
+    element: <App />,
+  },
+
+  {
+    path: "/pay",
+    element: <App />,
+  },*/
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />npm 
+  </React.StrictMode>,
+)
