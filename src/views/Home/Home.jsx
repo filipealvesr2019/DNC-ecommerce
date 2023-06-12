894dd7fb7d6498d1b756c654b0aee79240df3388
Filipe Alves <filipealvesrds2019@gmail.com>
2023-06-12 20:23:48 -0300

    cm

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './index.scss';
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <h1>Home</h1>
       <button>
         <Link to={'/products/1'}>Produto</Link>
