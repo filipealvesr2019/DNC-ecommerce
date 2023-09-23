5a138d0b8341bb5f5a55798b59672636c5d5a165
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-09-23 09:51:23 -0300

    cm

--- src/components/Header-menu/HeaderMenu.jsx
+++ src/components/Header-menu/HeaderMenu.jsx
@@ -5,22 +5,24 @@ import cartshop from "/src/assets/cartshop.png";
 const HeaderMenu = () => {
   return (
     <header className="header_menu">
-      <div className='container-left'>
-        <img src={logo} alt="" />
-      <ul>
-        <li>Home</li>
-        <li>Novidades</li>
-        <li>Feminino</li>
-        <li>Masculino</li>
-        <li>Atendimento</li>
-      </ul>
-      </div>
-      
-      <div className="header-menu__cart-shop">
-        <h1>Meu Carrinho</h1>
-        <img src={cartshop} alt="cartLogo" />
-
-      </div>
+       <div className='left-section'>
+        <img src={logo} alt="logo icone" />
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Novidades</li>
+            <li>Feminino</li>
+            <li>Masculino</li>
+            <li>Atendimento</li>
+          </ul>
+        </nav>
+       </div>
+       <div className='right-section'>
+        <div className='header-menu-logo'>
+          <span>Meu Carrinho</span>
+          <img src={cartshop} alt="cart shop icone" />
+        </div>
+       </div>
     </header>
   )
 }
