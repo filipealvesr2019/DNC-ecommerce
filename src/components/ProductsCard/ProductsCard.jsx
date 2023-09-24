fdb313d0e767f82d1ca5b75c92b6c6aad04772ff
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-09-23 22:23:26 -0300

    cm

--- src/components/ProductsCard/ProductsCard.jsx
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -1,32 +1,37 @@
-import { Link } from "react-router-dom"
-import "./index.scss"
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./index.scss";
+import React from "react";
 
-const ProductsCard = ({data}) => {
-  
+const ProductsCard = ({ data }) => {
   return (
     <div className="home__card">
       <div className="space"></div>
       <div className="card-container">
         <div className="image-container">
-        <img className="image" src={data.imgPath} alt="" /><img/>
+          <img className="image" src={data.imgPath} alt="" />
+          <img />
         </div>
-      
-        <div className="home__card-description" style={{
-          textAlign: "center",
-        justifyContent:"center",
-          border:"1px solid #ccc",
-       height:"15vh", borderRadius:"5px"}}>
-            <p>{data.title}</p>
-            <a href=""><button className="button">
-                <Link to={`/products/${data.id}`}>Comprar Look</Link>
-            </button></a>
-            
+
+        <div
+          className="home__card-description"
+          style={{
+            textAlign: "center",
+            justifyContent: "center",
+            border: "1px solid #ccc",
+            height: "15vh",
+            borderRadius: "5px",
+          }}
+        >
+          <p>{data.title}</p>
+          <a href="">
+            <button className="button">
+              <Link to={`/products/${data.id}`}>Comprar Look</Link>
+            </button>
+          </a>
         </div>
       </div>
-        
     </div>
-  )
-}
+  );
+};
 
-export default ProductsCard
+export default ProductsCard;
